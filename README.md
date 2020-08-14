a4bbe8bf13b16a09d5de99db837e623ba46989ce
Forrest Collins <Forest.D.Collins@gmail.com>
2020-08-14 03:39:10 -0500

    Adds Test Config and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# EZPackage
+
+A Tool geared towards making the building and publish of NPM packages as easy as possible.
---
+++ packages/index.ts
@@ -0,0 +1,4 @@
+export { BuildCommand } from './commands/Build'
+export { PublishCommand } from './commands/Publish'
+export { bundle } from './bundler/bundle'
+export { publish } from './publish/publish'
